16f9c4e6bc65cdea28ba7e4bb64ce78f34271a7d
sophie <sophie.human.k@gmail.com>
2022-03-04 17:39:23 +0900

    try css bundling

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
-// file exports (다른 js 파일을 불러와서 쓰기)
-const monkey = require("./monkey.js"); // 변수에 담아쓰면 된다. require 말고 import 방식으로도 가능하다.
+// JS file import (다른 js 파일을 불러와서 쓰기)
+const monkey = require('./monkey.js'); // 변수에 담아쓰면 된다. require 말고 import 방식으로도 가능하다.
 console.log(monkey);
+
+// CSS file import
+require('./css/main.css'); // webpack 설정까지하면, 번들링 시 불러와짐
 
 // sample code
 const components = () => {
-    const h1 = document.createElement("h1");
-    h1.innerText = "hello webpack!";
-    return h1;
-}
+  const h1 = document.createElement('h1');
+  h1.innerText = 'hello webpack!';
+  return h1;
+};
 
 document.body.appendChild(components());
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
-  mode: "production",
-  entry: "./src/app.js",
+  mode: 'production',
+  entry: './src/app.js',
   output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, "dist")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
-        test: /\.js?$/,
-        loader: "babel-loader",
+        test: /\.js$/,
+        use: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
-}
+};
